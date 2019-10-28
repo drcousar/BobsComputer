eaf1c46fd40c28e289f50a355775ede3df9be2f3
Jordan Hennessy <jorrdygirl@me.com>
2019-10-28 14:25:08 -0500

    Nav

--- src/app/shared/base-layout/base-layout.component.ts
+++ src/app/shared/base-layout/base-layout.component.ts
@@ -7,6 +7,7 @@
 ;===========================================
 */
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-base-layout',
@@ -15,9 +16,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BaseLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.router.navigate(['/session/login']);
   }
 
 }
