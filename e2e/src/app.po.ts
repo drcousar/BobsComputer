2c79b38abe8c3920be6ec5c6f3380052e03d326d
Alan Edwards <alan.edwards1103@gmail.com>
2019-11-04 20:54:26 -0500

    attribution

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,3 +1,9 @@
+/*; Title:  starter code
+; Author: Professor Krasso
+; Date:   21 October 2019
+; Description: BobComputer Starter Code
+;===========================================
+*/
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
--- src/app/pages/signup-dialog/signup-dialog.component.spec.ts
+++ src/app/pages/signup-dialog/signup-dialog.component.spec.ts
@@ -1,3 +1,9 @@
+/*; Title:  starter code
+; Author: Professor Krasso
+; Date:   21 October 2019
+; Description: BobComputer Starter Code
+;===========================================
+*/
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupDialogComponent } from './signup-dialog.component';
